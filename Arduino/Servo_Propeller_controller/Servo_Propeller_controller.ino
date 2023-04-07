f8cd2bc2c7ad53eef05610d336a73b7241c02120
LanceZhang <38578094+Lance-Zhang@users.noreply.github.com>
2023-04-08 02:48:40 +0800

    Save vrpn_client_ros package

--- Arduino/Servo_Propeller_controller/Servo_Propeller_controller.ino
+++ Arduino/Servo_Propeller_controller/Servo_Propeller_controller.ino
@@ -21,9 +21,9 @@ Servo propeller[4];
 Servo dockservo[4];
 
 // pin
-int proplPin[4] = {2,3,4,5};
-int servoPin[4] = {6,7,8,9};
-int detecPin[4] = {10,11,12,13};
+int proplPin[4] = {6,7,8,9};
+int servoPin[4] = {10,11,12,13};
+int detecPin[4] = {2,3,4,5};
 
 // functions
 void InitServo(){
@@ -37,29 +37,17 @@ void InitServo(){
     
     // Propeller
     propeller[i].attach(proplPin[i]);
+    propeller[i].write(90);
   }
 }
 
-void StartPropeller() {
-  //Serial.println("start propellers at:");
-  for (int pos = 0; pos <= 180; pos += 10) { // goes from 0 degrees to 180 degrees
-    // in steps of 1 degree
-    for (int i = 0; i < 4; i++)
-      propeller[i].write(pos);              // tell servo to go to position in variable 'pos'
-    //Serial.println(pos);
-    delay(300);                       // waits 10ms for the servo to reach the position
-  }
-  if (debug) Serial.println("Propellers are started!");
-}
-
-char buffer[6];
+char buffer[10];
 bool ser_flag;
 bool SerialRead() {
-  // read the serial port, Serial is comport, command length is 6 
-  Serial.readBytes(buffer,6);
-  // 
+  // read the serial port, Serial is comport, command length is 10
+  Serial.readBytes(buffer,10);
   // whether command is right. -1: propeller, -2: servo
-  if ((buffer[0] == -1 || buffer[0] == -2 ) && buffer[5] == -6) 
+  if ((buffer[0] == -1 || buffer[0] == -2 ) && buffer[9] == -6) 
   {
     if (debug) Serial.print("Serial read command:  ");
     ser_flag = true;
@@ -71,7 +59,7 @@ bool SerialRead() {
   }
   //
   if (debug) {
-    for(int i=0; i<6; i++)
+    for(int i=0; i<10; i++)
       {
         Serial.print(int(buffer[i]));
         Serial.print(", ");
@@ -82,17 +70,17 @@ bool SerialRead() {
 }
 
 int prop_pwm[4] = {0,0,0,0};
-int prop_pwm_bound[2] = {40, 140};
+int prop_pwm_bound[2] = {1000, 2000};
 void PropellerRun() {
   // propeller motor
   if (debug) Serial.print("PropellerRun: ");
   for (int i = 0; i < 4; i++) {
     if (debug) Serial.print(prop_pwm[i]);
-    int pwm = int(buffer[i+1]) + prop_pwm_bound[0];
+    int pwm = int(buffer[2*i+1])*100 + int(buffer[2*i+2]);
     // check if the same as last time
-    if(pwm != prop_pwm[i] && pwm >= prop_pwm_bound[0] && pwm <= prop_pwm_bound[1]) {
+    if((pwm >= prop_pwm_bound[0] && pwm <= prop_pwm_bound[1]) || pwm == 0) {
       prop_pwm[i] = pwm;
-      propeller[i].write(prop_pwm[i]);
+      propeller[i].writeMicroseconds(prop_pwm[i]);
       if (debug) Serial.print(" -> ");
       if (debug) Serial.print(prop_pwm[i]);
     }
@@ -101,34 +89,60 @@ void PropellerRun() {
   if (debug) Serial.print("\n");
 }
 
-void ServoRun() {
+void ServoRun() {  
   //
   if (debug) Serial.print("ServoRun: ");
   for (int i = 0; i < 4; i++) {
-    dockservo[i].write(int(buffer[i+1]));
-    if (debug) Serial.print(int(buffer[i+1]));
+    if (int(buffer[2*i+1]==0))
+      dockservo[i].write(0);
+
+    if (int(buffer[2*i+1]==1))
+      dockservo[i].write(180);
+      
+    if (debug) Serial.print(buffer[2*i+1]);
     if (debug) Serial.print(", ");
   }
   if (debug) Serial.print("\n");
+}
+
+int dock[4] = {0,0,0,0};
+void DockDetection(){
+  // feedback the docking detection
+  for(int i =0; i < 4; i++){
+    //Serial1.print(digitalRead(detecPin[i]));
+    dock[i] = digitalRead(detecPin[i]);
+    if (debug) {
+      Serial.println("pin "+ String(i) + ": " + String(dock[i]));
+      if (dock[i] == 0){
+        Serial.print(i);
+        Serial.print("connected");
+        }
+      if (dock[i] == 1){
+        Serial.print(i);
+        Serial.print("unconnected");
+        }
+      Serial.print("\n");
+    }
+  }
+  delay(10);
+  Serial1.print(String(dock[0])+String(dock[1])+String(dock[2])+String(dock[3])+"\n");
 }
 
 // Main
 void setup() {
   // communication port
   Serial.begin(115200);
-  Serial1.begin(115200); 
+  Serial1.begin(115200);
   
   while (!Serial || !Serial1) {}
   if (debug) Serial.println("Communication port is ready!");
   
   InitServo();
-  if (debug) Serial.println("servo control is ready!");
-  
-  StartPropeller();
-  if (debug) Serial.println("propeller control is ready!");
+  if (debug) Serial.println("Motors are ready!");
 }
 
 void loop() {
+  DockDetection();
   if(Serial.available()) {
     //
     if(SerialRead())
@@ -150,11 +164,5 @@ void loop() {
       }
       if (debug) Serial.print("\n");
     }
-  }
-  
-  // feedback the docking detection
-  for (int i = 0; i < 4; i++) {
-    Serial1.print(digitalRead(detecPin[i]));
-  }
-  Serial1.println("\n");
+  }  
 }
